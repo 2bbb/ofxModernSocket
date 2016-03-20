542c31d4f15f6b48cb2fae0c43f153a5a0fe2500
ISHII 2bit <ishii@buffer-renaiss.com>
2016-03-20 15:35:40 +0900

    add tag util

---
+++ src/osc/ofxModernOscTag.h
@@ -0,0 +1,18 @@
+//
+//  ofxModernOscTag.h
+//
+//  Created by ISHII 2bit on 2016/03/20.
+//
+//
+
+#pragma once
+
+#include <oscpp/detail/tag.h>
+
+namespace ofx {
+    namespace ModernOsc {
+        namespace Tag = OSCPP::Tag;
+        using OSCPP::TagType;
+        using OSCPP::TagName;
+    };
+};
