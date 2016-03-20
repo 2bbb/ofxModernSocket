58ac6ebc9446d7bef8642ccfc9e6073c6425aa78
ISHII 2bit <ishii@buffer-renaiss.com>
2016-03-20 20:25:19 +0900

    implement callback with prepare in receive thread.

--- src/osc/ofxModernOscReceiver.cpp
+++ src/osc/ofxModernOscReceiver.cpp
@@ -38,10 +38,9 @@ void Receiver::handle(const OSCPP::Server::Packet &packet) {
             handle(packets.next());
         }
     } else {
-        // Convert to message
         OSCPP::Server::Message msg(packet);
+        
         std::string address(msg.address());
-        // Get argument stream
         OSCPP::Server::ArgStream args(msg.args());
         
         ofxModernOscMessage mess(address);
@@ -77,8 +76,52 @@ void Receiver::handle(const OSCPP::Server::Packet &packet) {
                     break;
             }
         }
+        
         messageChannel.send(std::move(mess));
+        
+        auto it = thread_processes.find(address);
+        if(it != thread_processes.end()) for(std::size_t i = 0, num = thread_processes.count(address); i < num; ++it, i++) {
+            it->second->process(mess);
+        }
     }
 }
+
+void Receiver::update(ofEventArgs &) {
+    leakedMessages.clear();
+    ofxModernOscMessage mess;
+    while(messageChannel.tryReceive(mess)) {
+        std::size_t num = callbacks.count(mess.address);
+        auto it = callbacks.find(mess.address);
+        if(it != callbacks.end()) for(std::size_t i = 0, num = callbacks.count(mess.address); i < num; ++it, i++) {
+            it->second(mess);
+        } else {
+            if(!thread_processes.count(mess.address)) {
+                leakedMessages.push_back(std::move(mess));
+            }
+        }
+    }
+    for(auto process : thread_processes) process.second->call();
+}
+
+template <typename Prepared>
+void Receiver::addThreadCallback(const std::string &bindAddress,
+                       ThreadProcess<Prepared> thread_process,
+                       PreparedCallback<Prepared> callback)
+{
+    thread_processes.insert(std::make_pair(bindAddress, makeThreadedCallback(thread_process, callback)));
+}
+
+template <typename ThreadProcess, typename Callback>
+void Receiver::addThreadCallback(const std::string &bindAddress, ThreadProcess tp, Callback cb) {
+    static_assert(is_argument_message<ThreadProcess>(), "argument of ThreadProcess is not ofxModernOscMessage.");
+    static_assert(is_callable<ThreadProcess>::value, "ThreadProcess is not function.");
+    static_assert(is_callable<Callback>::value, "Callback is not function.");
+    static_assert(composable<ThreadProcess, Callback>(), "result of ThreadProcess is not argument of Callback.");
+    
+    addThreadCallback(bindAddress,
+                      static_cast<typename function_info<ThreadProcess>::function_type>(tp),
+                      static_cast<typename function_info<Callback>::function_type>(cb));
+}
+
 
 END_NAMESPACE_OFX_MODERN_OSC
--- src/osc/ofxModernOscReceiver.h
+++ src/osc/ofxModernOscReceiver.h
@@ -9,13 +9,18 @@
 
 #include <unordered_map>
 #include <vector>
-
-#include "ofxModernUDPReceiver.h"
-#include "ofxModernOscMessage.h"
+#include <memory>
+#include <type_traits>
 
 #include "ofThreadChannel.h"
 #include "ofEvent.h"
 #include "ofEvents.h"
+
+#include "ofxModernUDPReceiver.h"
+#include "ofxModernOscMessage.h"
+
+#include "../detail/ofx_modern_socket_type_utils.h"
+#include "./detail/ofxModernOscThreadedProcessUtils.h"
 
 namespace OSCPP {
     namespace Server {
@@ -32,25 +37,16 @@ namespace ofx {
         class Receiver : public ofxModernUDPReceiver {
             ofThreadChannel<ofxModernOscMessage> messageChannel;
             std::vector<ofxModernOscMessage> leakedMessages;
+            std::unordered_multimap<std::string, Callback> callbacks;
+            std::unordered_multimap<std::string, AbstractThreadedCallbackRef> thread_processes;
+            
         protected:
             virtual void receive(const boost::system::error_code &error_code,
                                  std::array<char, buf_size> &buf,
                                  std::size_t len) override;
             void handle(const OSCPP::Server::Packet &packet);
-            std::unordered_multimap<std::string, Callback> callbacks;
-            void update(ofEventArgs &) {
-                leakedMessages.clear();
-                ofxModernOscMessage mess;
-                while(messageChannel.tryReceive(mess)) {
-                    std::size_t num = callbacks.count(mess.address);
-                    auto it = callbacks.find(mess.address);
-                    if(it != callbacks.end()) for(std::size_t i = 0, num = callbacks.count(mess.address); i < num; it++, i++) {
-                        it->second(mess);
-                    } else {
-                        leakedMessages.push_back(std::move(mess));
-                    }
-                }
-            }
+            void update(ofEventArgs &);
+            
         public:
             Receiver() : ofxModernUDPReceiver() {
                 ofAddListener(ofEvents().update, this, &Receiver::update, OF_EVENT_ORDER_BEFORE_APP);
@@ -64,6 +60,16 @@ namespace ofx {
             void addCallback(const std::string &bindAddress, Callback callback) {
                 callbacks.insert(std::make_pair(bindAddress, callback));
             }
+            
+            template <typename Prepared>
+            void addThreadCallback(const std::string &bindAddress,
+                                   ThreadProcess<Prepared> thread_process,
+                                   PreparedCallback<Prepared> callback);
+            
+            template <typename ThreadProcess, typename Callback>
+            void addThreadCallback(const std::string &bindAddress,
+                                   ThreadProcess tp,
+                                   Callback cb);
             
             using iterator = std::vector<ofxModernOscMessage>::iterator;
             using const_iterator = std::vector<ofxModernOscMessage>::const_iterator;
