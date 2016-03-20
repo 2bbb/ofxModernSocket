35873d0707df730234b589d7f628f8b279bc8603
ISHII 2bit <ishii@buffer-renaiss.com>
2016-03-20 16:40:16 +0900

    implement basics

--- src/osc/ofxModernOscMessage.h
+++ src/osc/ofxModernOscMessage.h
@@ -15,10 +15,39 @@
 namespace ofx {
     namespace ModernOsc {
         class Message {
-            std::string address;
             std::vector<Argument> args;
         public:
+            const std::string address;
             
+            inline std::size_t size() const {
+                return args.size();
+            }
+            inline Argument operator[](std::size_t index) {
+                return args[index];
+            }
+            inline const Argument &operator[](std::size_t index) const {
+                return args[index];
+            }
+            
+            using iterator = std::vector<Argument>::iterator;
+            using const_iterator = std::vector<Argument>::const_iterator;
+            
+            iterator begin() { return args.begin(); }
+            iterator end() { return args.end(); }
+            const_iterator begin() const { return args.cbegin(); }
+            const_iterator end() const { return args.cend(); }
+            const_iterator cbegin() const { return args.cbegin(); }
+            const_iterator cend() const { return args.cend(); }
+            
+            using reverse_iterator = std::vector<Argument>::reverse_iterator;
+            using const_reverse_iterator = std::vector<Argument>::const_reverse_iterator;
+            
+            reverse_iterator rbegin() { return args.rbegin(); }
+            reverse_iterator rend() { return args.rend(); }
+            const_reverse_iterator rbegin() const { return args.crbegin(); }
+            const_reverse_iterator rend() const { return args.crend(); }
+            const_reverse_iterator crbegin() const { return args.crbegin(); }
+            const_reverse_iterator crend() const { return args.crend(); }
         };
     };
 };
